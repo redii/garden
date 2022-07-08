02d2f8627621aca5fd20eb5dd98ece8683fcf674
akmann <akmann.henry@gmail.com>
2022-07-08 12:22:41 +0200

    added test_camera.js

---
+++ scripts/test_camera.js
@@ -0,0 +1,36 @@
+require("dotenv").config({ path: "../.env" })
+const FormData = require("form-data")
+const fetch = require("node-fetch")
+const fs = require("fs")
+
+const PiCamera = require("pi-camera")
+const cam = new PiCamera({
+    mode: "photo",
+    width: 1920,
+    height: 1080,
+    output: "latest.jpg",
+    nopreview: true,
+})
+
+const date = new Date()
+const timestamp = date.toLocaleString("de-DE").replace(", ", "_")
+
+const form = new FormData()
+form.append("title", timestamp)
+form.append("description", `${timestamp.replace("_", " ")}`)
+form.append("album", process.env.IMGUR_ALBUM)
+form.append("image", fs.createReadStream("latest.jpg"))
+
+const response = await fetch("https://api.imgur.com/3/image", {
+    method: "POST",
+    headers: {
+        Authorization: `Bearer ${process.env.IMGUR_ACCESS_TOKEN}`,
+    },
+    body: form,
+})
+
+console.log("Image taken", response)
+
+fs.unlink("latest.jpg", (err) => {
+    if (err) console.log(err)
+})
